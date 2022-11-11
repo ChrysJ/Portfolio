75170924069359186fe8d2774f65e90268c36e33
ChrysJ <jouin.chrystopher@gmail.com>
2022-11-11 19:46:11 +0100

    first commit

---
+++ public/assets/data/experiencesData.js
@@ -0,0 +1,27 @@
+export const experiencesData = [
+  {
+    year: "2022",
+    workplace: "lamanu",
+    job: "- Formation développeur web et mobile",
+  },
+  {
+    year: "2022",
+    workplace: "udemy",
+    job: "- Formation en auto didacte SCSS / JAVASCRIPT / REACT",
+  },
+  {
+    year: "2021",
+    workplace: "piercing spirit",
+    job: "- Web designer",
+  },
+  {
+    year: "2021",
+    workplace: "lamanu",
+    job: "- Formation web design et communication digitale",
+  },
+  {
+    year: "2019",
+    workplace: "valeo",
+    job: "- Conducteur de machine industrielle",
+  },
+];
---
+++ public/assets/data/skillsData.js
@@ -0,0 +1,26 @@
+export const skillsData = [
+  {
+    title: "design",
+    item1: "UI / UX",
+    item2: "Branding",
+    item3: "Créations de logo",
+    item4: "Presentations",
+  },
+  {
+    title: "dev",
+    item1: "Sass / React",
+    item2: "Php",
+    item3: "Wordpress",
+    item4: "Veille technologique",
+    item5: "Bonne structuration du code",
+  },
+
+  {
+    title: "tools",
+    item1: "Github",
+    item2: "Figma",
+    item3: "Notion",
+    item4: "Affinity Designer",
+    item5: "Affinity Photo",
+  },
+];
